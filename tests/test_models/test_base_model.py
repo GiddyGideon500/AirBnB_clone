6eb3ba3f28549fd2f12422c496cf57e80c2d8f31
kalbetre-alx <kalbetre@icloud.com>
2022-10-26 21:09:00 +0300

    add doctest for base class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,11 +4,42 @@
 This Module contains a tests for Base Class
 """
 
+import inspect
 import unittest
 
+import pycodestyle
 from models import base_model
 
 Base = base_model.Base
+
+
+class TestBaseDocsAndStyle(unittest.TestCase):
+    """Tests Base class for documentation and style conformance"""
+
+    def test_pycodestyle(self):
+        """Tests compliance with pycodestyle"""
+        style = pycodestyle.StyleGuide(quiet=False)
+        result = style.check_files(
+            ["models/base_model.py", "tests/test_models/test_base_model.py"])
+        self.assertEqual(result.total_errors, 0)
+
+    def test_module_docstring(self):
+        """Tests whether the module is documented"""
+        self.assertTrue(len(base_model.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Tests whether the class is documented"""
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_methods_docstring(self):
+        """Tests whether the class methods are documented"""
+        funcs = inspect.getmembers(Base, inspect.isfunction)
+        for func in funcs:
+            self.assertTrue(len(func.__doc__) >= 1)
+
+    def test_class_name(self):
+        """Test whether the class name is correct"""
+        self.assertEqual(Base.__name__, "Base")
 
 
 class TestBase(unittest.TestCase):
