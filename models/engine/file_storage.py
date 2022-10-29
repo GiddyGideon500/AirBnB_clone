f8272ba6e3c0b5e5d65634f8537a9be1b089fbf7
Totweva <totweva@hmail.com>
2022-10-29 11:34:40 -0700

    commit adj FS

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,8 @@
 This Module contains a definition for FileStorage Class
 """
 import json
+import os
+
 from models.base_model import BaseModel
 
 
@@ -20,22 +22,22 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
-        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        self.__objects[obj.id] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        dctn = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
-            json.dump(dctn, f)
+        with open(self.__file_path, mode="w") as f:
+            f.write(json.dumps({k: v.to_dict()
+                for k, v in self.__objects.items()}))
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
-        try:
-            with open(FileStorage.__file_path, encoding='utf8') as f:
-                objects = json.load(f)
-        except FileNotFoundError:
-            FileStorage.__object = {}
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                content = f.read()
+                if len(content) > 0:
+                    self.__objects = {k: BaseModel(**v) for k, v in jason.loads(content).items()}
