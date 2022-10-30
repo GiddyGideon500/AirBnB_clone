76c506fa2171d21997265a9fee4cafea04ff842d
Totweva <totweva@hmail.com>
2022-10-30 01:57:43 -0700

    adj class

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Implementation of the amenity class"""
+
+from models.base_models import BaseModel
+
+
+class Amentity(BaseModel):
+    pass
--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@
 from models.base_model import BaseModel
 
 
-class state(BaseModel):
+class State(BaseModel):
     """
     Represents the state of the model
 
