1b3cfc356c33448bbe6be219b2b624547875bc02
Totweva <totweva@hmail.com>
2022-10-30 01:23:06 -0700

    started implement

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class user(BaseModel):
+    """Represent a User.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
